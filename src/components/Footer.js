dfd4d2cd51349306066d593337007fe1142f33df
Gbolahan Balogun <gbolahanbalogun5@gmail.com>
2020-10-28 13:54:05 +0100

    Add footer and its styles

---
+++ src/components/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className='text-center'>
+      <span>
+        Made by <a href='https://www.github.com/justsolomon'>Solomon.</a>
+      </span>
+      <span>
+        {' '}
+        Icon made by{' '}
+        <a
+          href='https://www.flaticon.com/authors/dinosoftlabs'
+          title='DinosoftLabs'
+        >
+          DinosoftLabs
+        </a>{' '}
+        from{' '}
+        <a href='https://www.flaticon.com/' title='Flaticon'>
+          www.flaticon.com
+        </a>
+      </span>
+    </footer>
+  );
+}
+
+export default Footer;
