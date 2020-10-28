f1dda608758566e7257657b87f05e7308280bbce
Gbolahan Balogun <gbolahanbalogun5@gmail.com>
2020-10-28 13:53:49 +0100

    Add footer to layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import '../assets/scss/app.scss';
+import Footer from './Footer';
 
 function Layout({ children }) {
   return (
     <div className='container'>
       <Header />
       {children}
+      <Footer />
     </div>
   );
 }
