c0f3e35bd119cb0dd428763eb1f1ebb43c293164
Gbolahan Balogun <gbolahanbalogun5@gmail.com>
2020-10-28 14:00:12 +0100

    Add target, rel and title attributes to attribution links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,19 +4,34 @@ function Footer() {
   return (
     <footer className='text-center'>
       <span>
-        Made by <a href='https://www.github.com/justsolomon'>Solomon.</a>
+        Made by{' '}
+        <a
+          href='https://www.github.com/justsolomon'
+          target='_blank'
+          title='Gbolahan Balogun'
+          rel='noreferrer'
+        >
+          Solomon.
+        </a>
       </span>
       <span>
         {' '}
         Icon made by{' '}
         <a
           href='https://www.flaticon.com/authors/dinosoftlabs'
+          target='_blank'
           title='DinosoftLabs'
+          rel='noreferrer'
         >
           DinosoftLabs
         </a>{' '}
         from{' '}
-        <a href='https://www.flaticon.com/' title='Flaticon'>
+        <a
+          href='https://www.flaticon.com/'
+          target='_blank'
+          title='Flaticon'
+          rel='noreferrer'
+        >
           www.flaticon.com
         </a>
       </span>
